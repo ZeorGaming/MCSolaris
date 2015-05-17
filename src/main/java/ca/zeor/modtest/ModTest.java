9d1903133d02c8dc00d473bf1966c771226506bb
Zeoic <zeorgaming@gmail.com>
2015-05-17 01:24:02 -0400

    Test
    test

--- src/main/java/ca/zeor/modtest/ModTest.java
+++ src/main/java/ca/zeor/modtest/ModTest.java
@@ -1,13 +1,12 @@
 package ca.zeor.modtest;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import ca.zeor.modtest.help.Reference;
 import ca.zeor.modtest.init.ModBlocks;
 import ca.zeor.modtest.init.ModItems;
-
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 
